4a0a54ddc0d65202ea99672dca38d764d0a5a0bd
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2019-07-10 14:34:24 +0300

    Configuring DAG task dependencies to form right flow for the pipeline.

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -67,3 +67,18 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
